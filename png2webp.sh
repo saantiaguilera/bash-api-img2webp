c02fde641fe2e5b69d5b8b05b1f26e535efd0f1a
saantiaguilera <santiago.aguilera@mercadolibre.com>
2017-05-09 16:58:25 -0300

    png2webp v2

--- png2webp.sh
+++ png2webp.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
+# For api customization please check https://developers.google.com/speed/webp/docs/cwebp
+
 WEBP_METHOD=6
 WEBP_QUALITY=70
 WORKING_DIR=$1
 
-if [ ! -d "$WORKING_DIRECTORY" ]; then
+if [ ! -d "$WORKING_DIR" ]; then
     echo "Please provide a directory."
     echo "If you want to run it for a single image please use:"
     echo "cwebp -q QUALITY(70) -m METHOD(6) INPUT -o OUTPUT"
     exit 1
 fi
 
-FILES=$(find "${WORKING_DIR}" -print | grep -E ".*\.(png|jpg|jpeg)$")
+FILES=($(find "${WORKING_DIR}" -print | grep -E "^[^\.]+\.(png|jpg|jpeg)$")) # This regex is done like this to avoid nine patchs.
 
 current_dir=""
 for ((i = 0; i < ${#FILES[@]}; i++)); do
     file="${FILES[$i]}"
-    echo "$file -> file"
     
     dir=$(echo "$file" | sed -E "s%(.*)/[a-zA-Z0-9\-_]+\.(png|jpg|jpeg)$%\1%g" | sed "s%/%_%g")
     name=$(echo "$file" | sed -E "s%.*/([a-zA-Z0-9\-_]+\.)(png|jpg|jpeg)$%\1webp%g")
@@ -24,11 +25,11 @@ for ((i = 0; i < ${#FILES[@]}; i++)); do
     # Check the current dir of the file exists, else create it.
     if [[ "$dir" != "$current_dir" ]]; then
         current_dir="$dir"
-        mkdir "$current_dir"
+        mkdir "$current_dir" >/dev/null
     fi
 
     echo "Creating: $name"
-    cwebp -q $WEBP_QUALITY -m $WEBP_METHOD "$file" -o "${current_dir}/$name" >/dev/null
+    cwebp -q $WEBP_QUALITY -m $WEBP_METHOD "$file" -o "${current_dir}/$name" -quiet
     echo "Created."
     echo ""
 done
